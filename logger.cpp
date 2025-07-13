f08c3ccb3a0078e5f1b2f0b277ee9c47f70630ef
Sa1ntJul1an <jtrman73739@gmail.com>
2025-07-13 14:07:48 -0600

    swap log level and logger name in log line

--- logger.cpp
+++ logger.cpp
@@ -80,7 +80,7 @@ void Logger::_logToOutputs(std::string logLine) {
 
 std::string Logger::_generateLogLine(std::string logMessage, int logLevel) {
   std::ostringstream oss;
-  oss << _loggerName << " | " << _logLevelMap[logLevel] << " | " << _getISOTimestamp() << " | " << logMessage;
+  oss << _logLevelMap[logLevel] << " | " << _loggerName << " | " << _getISOTimestamp() << " | " << logMessage;
   return oss.str();
 }
 
